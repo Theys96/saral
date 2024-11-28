ec40890cf443db00659905eb8f948d197fb90f90
Thijs Havinga <info@thijshavinga.nl>
2024-11-28 22:38:08 +0100

    Optimalisaties

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Check if the user has a stored preference and apply it
   if (localStorage.getItem("theme") == "light") {
     document.documentElement.classList.add("light-mode");
-    modeToggle.innerText = "🌕";
+    modeToggle.innerText = "🌑";
   } else {
     modeToggle.innerText = "☀️";
   }
@@ -15,22 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (document.documentElement.classList.contains("light-mode")) {
       localStorage.setItem("theme", "light");
-      modeToggle.innerText = "🌕";
+      modeToggle.innerText = "🌑";
     } else {
       modeToggle.innerText = "☀️";
       localStorage.removeItem("theme");
     }
     // Save the user's preference in a cookie or local storage if needed
   });
-
-
-const resumeButton = document.getElementsByClassName("resume");
-console.log(resumeButton);
-  for(let button of resumeButton){
-    button.addEventListener("click",() => {
-      window.open(
-        "https://drive.google.com/file/d/14RAXZV6gs-0wg2ZfMfqDbH-EHHMlFMdC/view?usp=sharing", "blank"
-        );
-    })
-  }
 });
